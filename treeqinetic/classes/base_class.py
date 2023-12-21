aa61464edb220e10e1117ab537ed976afc8d97a2
Kyell Jensen <mail@kyelljensen.de>
2023-12-21 10:53:13 +0100

    Last Commit vor der Implimentierung von Event-Listenern für CoreDataClass

--- treeqinetic/classes/base_class.py
+++ treeqinetic/classes/base_class.py
@@ -1,15 +1,25 @@
 from kj_core import get_logger
-
 import treeqinetic
 
 logger = get_logger(__name__)
 
 
-class BaseClass:
-    """
-    Base class built upon CoreBaseClass, using specific managers from treemotion.
-    """
+class BaseClass():
+    __abstract__ = True
+    _config = None
+    _plot_manager = None
 
     def __init__(self):
-        self.CONFIG = treeqinetic.CONFIG
-        self.PLOT_MANAGER = treeqinetic.PLOT_MANAGER
+        super().__init__()
+
+    @property
+    def CONFIG(self):
+        if self._config is None:
+            self._config = treeqinetic.CONFIG
+        return self._config
+
+    @property
+    def PLOT_MANAGER(self):
+        if self._plot_manager is None:
+            self._plot_manager = treeqinetic.PLOT_MANAGER
+        return self._plot_manager
