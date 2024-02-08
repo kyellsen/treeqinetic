14a415e6f55a9fc895d3d7aed963f66d67bd3d16
Kyell Jensen <mail@kyelljensen.de>
2024-02-08 15:17:13 +0100

    Some Refaktoring, Adding all db-classes to tcc

--- treeqinetic/classes/base_class.py
+++ treeqinetic/classes/base_class.py
@@ -16,10 +16,18 @@ class BaseClass():
     def CONFIG(self):
         if self._config is None:
             self._config = treeqinetic.CONFIG
-        return self._config
+        return self._config  # TODO: Try to delete
 
     @property
     def PLOT_MANAGER(self):
         if self._plot_manager is None:
             self._plot_manager = treeqinetic.PLOT_MANAGER
-        return self._plot_manager
+        return self._plot_manager  # TODO: Try to delete
+
+    @classmethod
+    def get_config(cls):
+        return treeqinetic.CONFIG
+
+    @classmethod
+    def get_plot_manager(cls):
+        return treeqinetic.PLOT_MANAGER
